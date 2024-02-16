d6433c3938e03d57c3edb684a98984fbe5a83d2a
ijonghyeon <academy3746@gmail.com>
2024-02-16 23:16:07 +0900

    Widgets Binding...

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:move_it/screens/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const ImageApp());
 }
 
