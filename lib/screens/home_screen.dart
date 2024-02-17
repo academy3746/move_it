ed643fb71a267af4adac76fba89900232ce7bfd0
ijonghyeon <academy3746@gmail.com>
2024-02-17 18:00:15 +0900

    Image Sliding

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,64 @@
+// ignore_for_file: avoid_print
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final PageController pageController = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer.periodic(
+      const Duration(seconds: 3),
+      (timer) async {
+        //print('Next Page!');
+
+        int? nextPage = pageController.page?.toInt();
+
+        if (nextPage == null) {
+          return;
+        }
+
+        if (nextPage == 4) {
+          nextPage = 0;
+        } else {
+          nextPage++;
+        }
+
+        await pageController.animateToPage(
+          nextPage,
+          duration: const Duration(milliseconds: 500),
+          curve: Curves.ease,
+        );
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(),
+      body: PageView(
+        controller: pageController,
+        children: [1, 2, 3, 4, 5]
+            .map(
+              (number) => Image.asset(
+                'assets/images/splash$number.jpeg',
+                fit: BoxFit.cover,
+              ),
+            )
+            .toList(),
+      ),
     );
   }
 }
